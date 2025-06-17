984475e8ba768c1419beed457665708cac64e47f
Mohammad Amin - Choopani <aminsec10@gmail.com>
2025-06-17 20:32:54 +0200

    add - parameter checking added to update info route

--- node/src/routes/user.ts
+++ node/src/routes/user.ts
@@ -6,6 +6,11 @@ const router = express.Router();
 
 //TODO: validate paramters in this route
 router.get("/info", accountController.showUserInfo);
-router.put("/info", accountController.updateUserInfo);
+router.put("/info", [
+    check(["username", "email"], {state: "failed", message: "Parameters are invalid or missing.", type: "input_error"}).isString().notEmpty(),
+    check("username", {state: "failed", message: "username must have at least 4 and maximum 24 character", type: "input_error"}).isLength({min: 4, max: 24}),
+    check("email", {state: "failed", message: "Invalid email address", type: "input_error"}).isEmail(),
+    checkThereIsAnyError
+], accountController.updateUserInfo);
 
 export default router;
