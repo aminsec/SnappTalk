d754634ed587152fcd83ae16ab64e59899f30cfb
Mohammad Amin - Choopani <aminsec10@gmail.com>
2025-07-08 20:01:09 +0200

    add: dead session validation added to jwt middleware

--- node/src/middlewares/jwt.ts
+++ node/src/middlewares/jwt.ts
@@ -1,6 +1,7 @@
 import * as jwt from "jsonwebtoken";
 import { Request, Response, NextFunction } from "express";
 import { ProtectedUserInfo } from "../types/user.types";
+import { getDeadSessionsCollection } from "../models/users.model";
 // import { Token } from "../types/user.types";
 
 //A middleware to validate JWT token
@@ -13,6 +14,14 @@ export default async function validateJWT(req: Request, resp: Response, next: Ne
 
   //Getting token from cookies
   const token = req.cookies.token;
+
+  //Checking if token is not in dead_sessions list
+  const dead_sessionsCL = await getDeadSessionsCollection();
+  const isTokenIsInDeadSessions = await dead_sessionsCL.findOne({token: token});
+  if(isTokenIsInDeadSessions){
+      resp.redirect("/login");
+      return;
+  }
 
   //Verifing token in try-catch. If token was not valid, it will go through an error and we handle it with catch
   try {
