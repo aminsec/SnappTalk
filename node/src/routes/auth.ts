ee69a2d62ddd4d7b1e59ed655ba80b58a163af49
Mohammad Amin - Choopani <aminsec10@gmail.com>
2025-07-15 13:48:37 +0200

    fix/ a message changed

--- node/src/routes/auth.ts
+++ node/src/routes/auth.ts
@@ -9,7 +9,7 @@ router.post("/", [
     check("password")
     .isString().withMessage("Invalid password value")
     .notEmpty().withMessage("Password is required")
-    .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]).{6,}$/).withMessage("Password is weak. It must contain at least one uppercase letter, one lowercase letter, one digit, and one special character."),
+    .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]).{6,}$/).withMessage("Incorrect or weak password"),
     checkThereIsAnyError
 ], handleAuth);
 
