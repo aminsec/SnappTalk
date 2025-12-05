e33a250169c0c0362b0dc417b38461cf40cae3f2
Mohammad Amin - Choopani <aminsec10@gmail.com>
2025-12-05 15:14:12 +0100

    rate limit in backend increased.

--- node/src/app.ts
+++ node/src/app.ts
@@ -12,8 +12,8 @@ import { rateLimit } from 'express-rate-limit'
 
 //Rate limit config 
 const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // Limit each IP to 100 requests per `windowMs`
+  windowMs: 10 * 60 * 1000, // 15 minutes
+  max: 300, // Limit each IP to 100 requests per `windowMs`
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 });
