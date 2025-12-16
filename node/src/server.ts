0d5b6121c767bb32e746c968819c40d087b3c99f
Mohammad Amin - Choopani <aminsec10@gmail.com>
2025-12-16 11:27:02 +0100

    first step of ws. it's working

--- node/src/server.ts
+++ node/src/server.ts
@@ -1,7 +1,7 @@
 import { connectToSnappTalkDB } from "./config/database";
 import http from "http";
-import WebSocket, { Server as WebSocketServer } from "ws";
 import app from "./app";
+import { Server } from 'socket.io';
 
 const PORT = Number(process.env.APP_PORT) || 2020;
 const server = http.createServer(app);
@@ -11,15 +11,31 @@ try {
   connectToSnappTalkDB();
 
   // WebSocket Setup
-  const wss = new WebSocketServer({ server, path: "/chat" }); // Binds WS to HTTP
+  const io = new Server(server, {
+    cors: {
+      origin: 'http://snapptalk.io:3000/', // React app
+      methods: ['GET', 'POST']
+    }
+  });
 
-  wss.on("connection", (client: WebSocket) => {
-    // handelWSC(client, wss);
-    console.log("New client connected");
+
+  io.on('connection', (socket) => {
+    console.log('New client connected:', socket.id);
+  
+    // Listen for events from the client
+    socket.on('message', (data) => {
+      console.log('Message received:', data);
+
+    });
+  
+    socket.on('disconnect', () => {
+      console.log('Client disconnected:', socket.id);
+    });
+
   });
   
 } catch (error) {
-  console.error("Couldn't start WS server", error);
+  console.error("System error accoured while starting one of servers", error);
 };
 
 server.listen(PORT, () => {
