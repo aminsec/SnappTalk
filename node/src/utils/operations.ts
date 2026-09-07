a900a69959f4fcc106e5517ad043081c1b44868b
Mohammad Amin - Choopani <aminsec10@gmail.com>
2026-09-07 11:37:36 +0200

    unused functions removed

--- node/src/utils/operations.ts
+++ node/src/utils/operations.ts
@@ -4,7 +4,6 @@ import { s3Client } from "../config/s3.minio";
 import { DeleteObjectCommand } from "@aws-sdk/client-s3";
 import { Response } from 'express';
 import { ProtectedUserInfo, RawUserInfo } from '../types/user.types';
-import * as fs from 'fs';
 import * as jwt from "jsonwebtoken";
 import { Conversation } from '../types/conversation.types';
 import { Message } from '../types/messages.types';
@@ -77,30 +76,6 @@ export function getRandomString(): string {
         result += chars.charAt(Math.floor(Math.random() * chars.length));
     }
     return result;
-};
-
-export async function uploadFile(content: string): Promise<[string | null, ErrorResponse | null]> {
-    // Decoding the base64 to save in buffer
-    const file: Buffer = Buffer.from(content, "base64");
-
-    // Writing the binary into a file in /uploads folder
-    try {
-        // Avoiding using file extention for security reasons
-        const filename: string = getRandomString();
-        const uploadPath = "/up/node/uploads/" + filename;
-        fs.writeFile(uploadPath, file, err => {
-            if(err){
-                throw new Error("System error occurred. Coudln't upload file");
-            }
-        });
-
-        return [filename, null];
-
-    } catch (error) {
-        console.log(error);
-        const err: ErrorResponse = {message: "A system error occurred. Couldn't upload file", state: "failed", type: "system_error"};
-        return [null, err];
-    }
 };
 
 export async function deleteFileFromS3(fileKey: string, bucketName: string): Promise<[Boolean | null, ErrorResponse | null]>  {
