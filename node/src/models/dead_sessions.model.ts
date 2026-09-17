e98fea8631374944157b5550f4339eb7e8420597
Mohammad Amin - Choopani <aminsec10@gmail.com>
2026-09-17 08:00:31 +0200

    1. Incorrect field name fixed. createdAt -> created_at 2. Indexing for dead sessions declared

--- node/src/models/dead_sessions.model.ts
+++ node/src/models/dead_sessions.model.ts
@@ -2,8 +2,28 @@ import mongoose, { Schema, Model } from "mongoose";
 import { DBDeadSessionsType } from "../types/token.types";
 
 const deadSessionSchema = new Schema<DBDeadSessionsType>({
-    token: { type: String, required: true },
-    createdAt: { type: Date, default: Date.now }
+    token: {
+        type: String,
+        required: true
+    },
+
+    created_at: {
+        type: Date,
+        default: Date.now
+    }
 });
 
-export const DeadSession: Model<DBDeadSessionsType> = mongoose.model<DBDeadSessionsType>("DeadSession", deadSessionSchema);
+// Simple lookup by token
+deadSessionSchema.index({
+    token: 1
+});
+
+// Delete sessions 24 hours after createdAt
+deadSessionSchema.index(
+    { created_at: 1 },
+    {
+        expireAfterSeconds: 60 * 60 * 24
+    }
+);
+
+export const DeadSession: Model<DBDeadSessionsType> = mongoose.model<DBDeadSessionsType>("dead_sessions", deadSessionSchema);
