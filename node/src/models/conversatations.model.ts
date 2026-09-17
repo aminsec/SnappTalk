1847ed5e9c8069efb60c41388510d0f15022368f
Mohammad Amin - Choopani <aminsec10@gmail.com>
2026-09-17 08:03:31 +0200

    Indexes declared for conversation collection

--- node/src/models/conversatations.model.ts
+++ node/src/models/conversatations.model.ts
@@ -2,13 +2,59 @@ import mongoose, { Schema, Model } from "mongoose";
 import { DBConversationType } from "../types/conversation.types";
 
 const conversationSchema = new Schema<DBConversationType>({
-    type: { type: String, enum: ["group", "pv"], required: true },
-    group_name: { type: String, default: null, unique: true },
-    group_avatar: { type: String, default: null },
-    members: [{ type: Schema.Types.ObjectId, ref: "User" }],
-    last_message_id: { type: Schema.Types.Mixed, default: {} },
-    deleted_for: { type: Schema.Types.Mixed, default: {} },
-    created_at: { type: Date, default: Date.now }
+    type: {
+        type: String,
+        enum: ["group", "pv"],
+        required: true
+    },
+
+    group_name: {
+        type: String,
+        default: null
+    },
+
+    group_avatar: {
+        type: String,
+        default: null
+    },
+
+    members: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "User"
+        }
+    ],
+
+    last_message_id: {
+        type: Schema.Types.Mixed,
+        default: {}
+    },
+
+    deleted_for: {
+        type: Schema.Types.Mixed,
+        default: {}
+    },
+
+    created_at: {
+        type: Date,
+        default: Date.now
+    }
 });
+
+// Find conversations belonging to a user
+conversationSchema.index({
+    members: 1
+});
+
+// Unique group names
+conversationSchema.index(
+    { group_name: 1 },
+    {
+        unique: true,
+        partialFilterExpression: {
+            group_name: { $type: "string" }
+        }
+    }
+);
 
 export const Conversation: Model<DBConversationType> = mongoose.model<DBConversationType>("Conversation", conversationSchema);
